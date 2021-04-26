9e03ba124542b129eafe11d984e5de6994b98683
pranaybattu <battupranay@gmail.com>
2021-04-26 17:03:58 +0530

    animated mobile Menu with css/js

--- app/js/script.js
+++ app/js/script.js
@@ -4,18 +4,30 @@ const header = document.querySelector('.header');
 
 const overlay = document.querySelector('.overlay');
 
+const fadeElems = document.querySelectorAll('.has-fade');
+
+const body =document.querySelector('body');
+
 btnHamburger.addEventListener('click',function(){
     console.log('love');
 
     if(header.classList.contains('open')) { //closes Hamberger menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
+        
     }
 
     else {// opens Hamberger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
+        
     }
 });
