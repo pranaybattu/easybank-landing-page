245d4c246105e3325eefcd8918f123a677dff509
pranaybattu <battupranay@gmail.com>
2021-04-26 10:36:45 +0530

    animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log('Heyy you!!')
+const btnHamburger = document.querySelector('#btnHamburger');
+
+const header = document.querySelector('.header'); 
+
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click',function(){
+    console.log('love');
+
+    if(header.classList.contains('open')) { //closes Hamberger menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+
+    else {// opens Hamberger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});
